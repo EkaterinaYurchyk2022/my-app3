e05fdc0a65563582a7f510440a93f240696e9833
EkaterinaYurchyk2022 <98664770+EkaterinaYurchyk2022@users.noreply.github.com>
2022-10-31 20:05:57 +0300

    Update Main.tsx

--- src/Main.tsx
+++ src/Main.tsx
@@ -8,8 +8,10 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+
 // const Stack = createNativeStackNavigator<RootStackParamList>();
 // const Drawer = createDrawerNavigator<RootStackParamList>();
+
 const Tab = createBottomTabNavigator<RootStackParamList>();
 
 
